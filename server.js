d63078b0a14c5f87fa20f1b488abe9378ba1ce45
Marwan Mai <emef4z@gmail.com>
2025-10-10 15:57:35 +0200

    Add use of html pages

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 // console.log(__filename); // Check filename
 // console.log(__dirname); // Check dirname
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   //   res.write("Assalamu'alaikum");
 
   //   res.setHeader("Content-Type", "text/html");
@@ -26,17 +26,27 @@ const server = http.createServer((req, res) => {
     // Check if GET request
     if (req.method === "GET") {
       // Create a router
-      let filePath;
+
+      let filePath; // file path for pages
+
       if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>Home Page - Assalamu'alaikum</h1>"); // res.end can be used to pass a message
+        // res.writeHead(200, { "Content-Type": "text/html" });
+        // res.end("<h1>Home Page - Assalamu'alaikum</h1>"); // res.end can be used to pass a message
+        filePath = path.join(__dirname, "public", "index.html");
       } else if (req.url === "/about") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>About Page - Assalamu'alaikum Warahmatullah</h1>"); // res.end can be used to pass a message
+        // res.writeHead(200, { "Content-Type": "text/html" });
+        // res.end("<h1>About Page - Assalamu'alaikum Warahmatullah</h1>"); // res.end can be used to pass a message
+        filePath = path.join(__dirname, "public", "about.html");
       } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h1>Page Not Found</h1>"); // res.end can be used to pass a message
+        // res.writeHead(404, { "Content-Type": "text/html" });
+        // res.end("<h1>Page Not Found</h1>"); // res.end can be used to pass a message
+        throw new Error("Not found");
       }
+
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
     } else {
       throw new Error("Method not allowed");
     }
