2e2a655f3ba676d73497844a2eb6bae4f5a1f56d
Marwan Mai <emef4z@gmail.com>
2025-10-10 13:39:12 +0200

    Add nodemon and .gitignore

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+import http from "http";
+
+const PORT = 8000;
+
 const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.statusCode = 404;
-  });
-  
+  //   res.write("Assalamu'alaikum");
+
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.statusCode = 404;
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Assalamu'alaikum</h1>"); // res.end can be used to pass a message
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on  port ${PORT}`);
+});
