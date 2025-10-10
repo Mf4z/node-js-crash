4b5b09a7a35ecc28863803e8e75e73ca3208f65d
Marwan Mai <emef4z@gmail.com>
2025-10-10 13:51:18 +0200

    Check the req.url and req.method methods

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const server = http.createServer((req, res) => {
 
   //   res.setHeader("Content-Type", "text/html");
   //   res.statusCode = 404;
+  console.log(req.url);
+  console.log(req.method);
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Assalamu'alaikum</h1>"); // res.end can be used to pass a message
 });
+
+
 
 server.listen(PORT, () => {
   console.log(`Server running on  port ${PORT}`);
