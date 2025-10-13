c8b0a9887ff9b26ae6cca0e577d94d7fdc1485be
Marwan Mai <emef4z@gmail.com>
2025-10-13 23:52:59 +0200

    Add and use path modules

--- pathDemo.js
+++ pathDemo.js
@@ -1,4 +1,5 @@
 import path from "path";
+import url from "url";
 
 const filePath = "./dir1/dir2/test.txt";
 
@@ -17,3 +18,18 @@ console.log(extname);
 // parse() - Gives the all info about the dir and path as an object
 const parse = path.parse(filePath);
 console.log(parse);
+
+// Getting __filename
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename);
+console.log(__dirname);
+
+// join() - Joins and constructs the path
+const filePath2 = path.join(__dirname, "dir1", "dir2", "dir3");
+console.log(filePath2);
+
+//  resolve - Resolves the path that uses / or \ | Get absolute path
+const filePath3 = path.resolve(__dirname, "dir1", "dir2", "dir3");
+console.log(filePath3);
