5d5871be705d8cea5734a93b9db60d8f37e1141b
Marwan Mai <emef4z@gmail.com>
2025-10-18 16:10:18 +0200

    Add the os module and work with it

---
+++ osDemo.js
@@ -0,0 +1,13 @@
+import os from "os";
+
+// User info
+console.log(os.userInfo());
+
+// Total memory
+console.log(os.totalmem());
+
+// Free memory
+console.log(os.freemem());
+
+// CPU info
+console.log(os.cpus());
